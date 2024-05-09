c661618b41262ab3a581be11388b2f7c03b204ab
Nandini Kaur <160272450+Nandinikaur@users.noreply.github.com>
2024-05-09 09:02:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+//Nandini_kaur - 30-04-2024
+import inquirer from "inquirer";
+import chalk from "chalk";
+let currencies = {
+    "USD": 1,
+    "EUR": 0.93,
+    "AED": 3.67,
+    "CAD": 1.37,
+    "AUD": 1.52,
+    "JPY": 155,
+    "CNY": 7.23,
+    "PKR": 278.22,
+    "INR": 83.47,
+    "RUB": 91.29, //Russian Ruble
+};
+let answer = await inquirer.prompt([
+    {
+        name: "amount",
+        type: "input",
+        message: chalk.bold.blue("Enter the amount to convert:")
+    },
+    {
+        name: "fromCurrency",
+        type: "list",
+        message: chalk.bold.magenta("covert from:"),
+        choices: ["USD", "EUR", "AED", "CAD", "AUD", "JPY", "CNY", "PKR", "INR", "RUB"]
+    },
+    {
+        name: "toCurrency",
+        type: "list",
+        message: chalk.bold.magenta("convert to:"),
+        choices: ["USD", "EUR", "AED", "CAD", "AUD", "JPY", "CNY", "PKR", "INR", "RUB"]
+    },
+]);
+let fromAmount = currencies[answer.fromCurrency];
+let toAmount = currencies[answer.toCurrency];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(chalk.bold.yellow(`Converted Amount = "${chalk.italic.cyanBright(convertedAmount)}"`));
